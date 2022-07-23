9d90b95b181722eb1fbc9c93727d2de7ad1b5bda
Nikos Katsikanis <nkatsikanis@gmail.com>
2022-07-23 11:05:45 +0300

    feat: simple hash routing working

--- src/components/router.js
+++ src/components/router.js
@@ -11,34 +11,35 @@ export default (hostComponent) => {
     hostComponent.querySelectorAll("[data-component='route']")
   );
 
-  // find the route that matches the url
-  const route = findHashUrlRoute(routes);
-  // if no route matches the url, return
-  if (!route) return;
-  displayRoute(route);
-
   //add hash listener to the host component
-  hostComponent.addEventListener("hashchange", () => {
-    route.dataset.active = "false";
-    //hide the old route
-    route.style.display = "none";
+  window.addEventListener("hashchange", () => {
+    displayMatchingRoute();
   });
-};
-const route = (hostComponent, path, handler) => {
-  const initialContent = hostComponent.innerHTML;
-};
 
-//display the route content
-const displayRoute = (route) => {
-  //mark found route as active
-  route.dataset.active = "true";
-  //enable the active route to be visible
-  route.style.display = "initial";
-};
+  const findHashUrlRoute = (routes) => {
+    let url = window.location.hash.split("#")[1];
+    if (!url) {
+      url = "";
+    }
 
-//find the active route
-const findHashUrlRoute = (routes) => {
-  const url = window.location.hash.split("#")[1];
+    return routes.find((route) => route.getAttribute("data-path") === url);
+  };
 
-  return routes.find((route) => route.getAttribute("data-path") === url);
+  const displayMatchingRoute = () => {
+    const newActiveRoute = findHashUrlRoute(routes);
+
+    // get the current active route from the dom and hide it
+    const activeRoute = hostComponent.querySelector("[data-active='true']");
+    if (activeRoute) {
+      activeRoute.dataset.active = "false";
+      //hide the old route
+      activeRoute.style.display = "none";
+    }
+    newActiveRoute.dataset.active = "true";
+    //enable the active route to be visible
+    newActiveRoute.style.display = "initial";
+  };
+
+  //first pass to display the first route
+  displayMatchingRoute();
 };
