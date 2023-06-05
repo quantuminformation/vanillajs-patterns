55b5068d43fa7f3fe7447307d90185763e4d82e4
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-06-05 18:29:50 +0200

    add first road marking and perspective

--- components/3d-road.js
+++ components/3d-road.js
@@ -1,62 +1,105 @@
 export default (hostComponent) => {
-  // Get the WebGL context
   const canvas = hostComponent.appendChild(document.createElement('canvas'));
   const gl = canvas.getContext('webgl');
 
-  // Create shaders
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, `
-    attribute vec2 position;
-    void main() {
-      gl_Position = vec4(position, 0.0, 1.0);
-    }
-  `);
-  gl.compileShader(vertexShader);
+  // Define the positions for the vertices
+  const positions = [
+    -1.0, -1.0, -5.0,   // Back left
+    1.0, -1.0, -5.0,   // Back right
+    -1.0, -1.0,  5.0,   // Front left
+    1.0, -1.0,  5.0,   // Front right
+  ];
 
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, `
-    void main() {
-      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-  `);
-  gl.compileShader(fragmentShader);
-
-  // Create program
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-
-  // Set up the positions attribute
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    -0.5, -0.5,
-    0.5, -0.5,
-    -0.5, 0.5,
-    -0.5, 0.5,
-    0.5, -0.5,
-    0.5, 0.5
-  ]), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-  const positionLocation = gl.getAttribLocation(program, 'position');
+  // Vertex shader program
+  const vsSource = `
+    attribute vec3 aVertexPosition;
+    uniform mat4 uProjectionMatrix;
+    void main() {
+      gl_Position = uProjectionMatrix * vec4(aVertexPosition, 1.0);
+    }
+  `;
+
+  // Fragment shader program
+  const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+  // Create shader program
+  const shaderProgram = createShaderProgram(gl, vsSource, fsSource);
+  gl.useProgram(shaderProgram);
+
+  // Look up where the vertex data needs to go.
+  const positionLocation = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
   gl.enableVertexAttribArray(positionLocation);
-  gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
 
-  // Draw
-  const resize = () => {
-    canvas.width = hostComponent.clientWidth;
-    canvas.height = hostComponent.clientHeight;
-    gl.viewport(0, 0, canvas.width, canvas.height);
-  };
-  window.addEventListener('resize', resize);
-  resize();
+  // Set the clear color to black
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
 
-  const render = () => {
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.useProgram(program);
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-    requestAnimationFrame(render);
-  };
-  render();
+  // Compute the projection matrix
+  const fieldOfView = 45 * Math.PI / 180; // in radians
+  const aspect = canvas.clientWidth / canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+  const projectionMatrix = perspective(fieldOfView, aspect, zNear, zFar);
+
+  // Set the projection matrix
+  const projectionMatrixLocation = gl.getUniformLocation(shaderProgram, 'uProjectionMatrix');
+  gl.uniformMatrix4fv(projectionMatrixLocation, false, projectionMatrix);
+
+  // Draw the road
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 };
+
+// Function to create shader program
+function createShaderProgram(gl, vsSource, fsSource) {
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    console.error('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+// Function to create shader
+function createShader(gl, type, source) {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+// Function to create perspective matrix
+function perspective(fieldOfViewInRadians, aspect, near, far) {
+  var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
+  var rangeInv = 1.0 / (near - far);
+  return [
+    f / aspect, 0, 0, 0,
+    0, f, 0, 0,
+    0, 0, (near + far) * rangeInv, -1,
+    0, 0, near * far * rangeInv * 2, 0
+  ];
+}
