2e3ab9ef8dec7188cb92d5c623f3505918366ee9
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-06-13 19:34:32 +0200

    form edit/view mode
    cancel back to pervious form values

--- components/form-user-signup.js
+++ components/form-user-signup.js
@@ -1,4 +1,7 @@
 export default (hostComponent) => {
+  let editMode = false;
+  let userDataSnapshot = null;
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -11,24 +14,77 @@ export default (hostComponent) => {
       email: formData.get("email"),
     };
 
-    window.appStore.setCurrentUser((currentUser) => {
+    window.appStore.setCurrentUser(() => {
       return user;
     });
+
     form.reset();
+    editMode = !editMode;
+    renderForm();
   };
 
-  hostComponent.innerHTML = `
-    <h2>Sign Up</h2>
-    <form>
-      <div class="form-grid">
-        <input type="text" id="firstName" name="firstName" placeholder="First Name" value="John" required />
-        <input type="text" id="lastName" name="lastName" placeholder="Last Name" value="Doe" required />
-        <input type="email" id="email" name="email" placeholder="Email" value="johndoe@example.com" required />
-        <button type="submit">Submit</button>
-      </div>
-    </form>
-  `;
+  const renderForm = () => {
+    const currentUser = window.appStore.getCurrentUser();
 
-  const form = hostComponent.querySelector("form");
-  form.addEventListener("submit", handleSubmit);
+    let userFields = currentUser
+        ? {
+          firstName: currentUser.firstName,
+          lastName: currentUser.lastName,
+          email: currentUser.email,
+        }
+        : {
+          firstName: "",
+          lastName: "",
+          email: "",
+        };
+
+    if (editMode) {
+      // Save snapshot of user's data
+      userDataSnapshot = {...userFields};
+
+      hostComponent.innerHTML = `
+        <h2>User Profile</h2>
+        <form>
+          <div class="flex gap-4 mb-4 flex-wrap">
+            <input type="text" id="firstName" name="firstName" placeholder="First Name" required value="${userFields.firstName}" />
+            <input type="text" id="lastName" name="lastName" placeholder="Last Name" required value="${userFields.lastName}" />
+            <input type="email" id="email" name="email" placeholder="Email" required value="${userFields.email}" />
+          </div>
+          <button type="submit">Submit</button>
+          <button type="button" id="cancel-edit">Cancel</button>
+        </form>
+      `;
+
+      const form = hostComponent.querySelector("form");
+      form.addEventListener("submit", handleSubmit);
+
+      const cancelEditButton = hostComponent.querySelector("#cancel-edit");
+      cancelEditButton.addEventListener("click", () => {
+        // On cancel, restore the user's data from the snapshot
+        window.appStore.setCurrentUser(() => {
+          return userDataSnapshot;
+        });
+        editMode = false;
+        renderForm();
+      });
+    } else {
+      hostComponent.innerHTML = `
+        <h2>User Profile</h2>
+        <div class="flex gap-4 mb-4 flex-wrap">
+          <div>${userFields.firstName}</div>
+          <div>${userFields.lastName}</div>
+          <div>${userFields.email}</div>
+        </div>
+        <button type="button" id="edit-profile">Edit</button>
+      `;
+
+      const editButton = hostComponent.querySelector("#edit-profile");
+      editButton.addEventListener("click", () => {
+        editMode = true;
+        renderForm();
+      });
+    }
+  };
+
+  renderForm();
 };
