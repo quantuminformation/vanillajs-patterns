4888944d199f84e63fc25f2502e8ee24deedcafb
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-07-11 13:29:21 +0300

    tidy css

--- components/theme-switcher.js
+++ components/theme-switcher.js
@@ -2,18 +2,32 @@
 export default (hostComponent) => {
   const themes = ['green', 'red', 'blue', 'grey']; // added 'grey'
 
-  themes.forEach(theme => {
-    const button = document.createElement('button');
-    button.textContent = theme.charAt(0).toUpperCase() + theme.slice(1);
+  const themeButtons = themes
+    .map(
+      (theme) => `
+    <button class="secondary" data-theme="${theme}">
+      ${theme.charAt(0).toUpperCase() + theme.slice(1)}
+    </button>
+  `
+    )
+    .join('');
 
-    button.addEventListener('click', function() {
-      themes.forEach(otherTheme => {
+  hostComponent.innerHTML = `
+    <div class="flex gap-md align-center">
+     <span>Theme</span> ${themeButtons}
+    </div>
+    
+  `;
+
+  themes.forEach((theme) => {
+    const button = hostComponent.querySelector(`button[data-theme="${theme}"]`);
+
+    button.addEventListener('click', function () {
+      themes.forEach((otherTheme) => {
         document.documentElement.classList.remove(otherTheme + '-theme');
       });
 
       document.documentElement.classList.add(theme + '-theme');
     });
-
-    hostComponent.appendChild(button);
   });
 };
