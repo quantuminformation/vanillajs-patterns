bab0b1fe957aaa07d44dfa2c5bcc903d6b914988
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-09-14 18:49:10 +0300

    cors fix

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,22 @@
 import { db } from './db';
 import { user } from 'db/schema';
 
-debugger;
 const server = Bun.serve({
   port: 8080,
   async fetch(req) {
-    const result = await db.select().from(user);
-    return new Response(JSON.stringify(result));
+    debugger;
+    const url = new URL(req.url);
+
+    const responseInit: ResponseInit = {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    };
+    if (url.pathname === '/users') {
+      const result = await db.select().from(user);
+      return new Response(JSON.stringify(result), responseInit);
+    }
+    return new Response('404!');
   },
 });
 
--- routes/users.js
+++ routes/users.js
@@ -2,49 +2,26 @@
 
 export default (hostComponent) => {
   const updateUsers = async () => {
-    // Fetch users from your API
-    const response = await fetch('http://localhost:8000/users');
-    const users = await response.json();
+    try {
+      // Note the change in the port from 8000 to 8080 to match your backend setup
+      const response = await fetch('http://localhost:8080/users');
+      if (!response.ok) throw new Error("Couldn't fetch users");
 
-    // Clear the host component
-    hostComponent.innerHTML = '';
+      const users = await response.json();
+      hostComponent.innerHTML = ''; // Clear the host component
 
-    // Append each user to the host component
-    for (const user of users) {
-      const userElement = document.createElement('p');
-      userElement.textContent = `ID: ${user.id}, Name: ${user.name}`;
-      hostComponent.appendChild(userElement);
+      // Append each user to the host component
+      for (const user of users) {
+        const userElement = document.createElement('p');
+        userElement.textContent = `ID: ${user.id}, Name: ${user.name}`;
+        hostComponent.appendChild(userElement);
+      }
+    } catch (error) {
+      console.error(error);
+      hostComponent.textContent = 'Failed to load users.';
     }
   };
 
-  const createUser = async (name) => {
-    // Post user data to your API
-    await fetch('http://localhost:8000/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name }),
-    });
-
-    // Update the user list after a user has been created
-    updateUsers();
-  };
-
-  // Add a form to the host component
-  const formElement = document.createElement('form');
-  formElement.innerHTML = `
-    <input type="text" id="name" name="name" placeholder="Enter user name" required>
-    <input type="submit" value="Submit">
-  `;
-  formElement.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const name = formElement.elements.name.value;
-    await createUser(name);
-    formElement.elements.name.value = '';
-  });
-  hostComponent.appendChild(formElement);
-
-  // Update the user list immediately
+  // Fetch and display the user list immediately
   updateUsers();
 };
