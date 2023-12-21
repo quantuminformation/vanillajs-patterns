28de8e60278b20827b2c707c89869f9f860c2650
Nikos Katsikanis <nkatsikanis@gmail.com>
2023-12-21 12:26:05 +0100

    allow month change

--- js/routes/time-tracker.js
+++ js/routes/time-tracker.js
@@ -1,75 +1,104 @@
 // stored in /js/routes/time-tracker.js
 
 export default (hostComponent) => {
-  hostComponent.innerHTML = ''; // Clear any existing content
+  let currentDate = new Date();
 
-  const currentDate = new Date();
-  const currentDay = currentDate.getDate();
-  const month = currentDate.getMonth();
-  const year = currentDate.getFullYear();
+  const updateCalendar = (hostComponent, currentDate) => {
+    hostComponent.innerHTML = ''; // Clear any existing content
 
-  // Get the first day of the month
-  const firstDayOfMonth = new Date(year, month, 1).getDay();
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
 
-  // Calculate the number of days in the month
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
+    // Get the first day of the month
+    const firstDayOfMonth = new Date(year, month, 1).getDay();
 
-  // Get browser's locale for formatting
-  const browserLocale = navigator.language;
+    // Calculate the number of days in the month
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-  // Generate weekdays based on the locale, starting with Sunday
-  const weekdays = [...Array(7).keys()].map((i) =>
-    new Intl.DateTimeFormat(browserLocale, { weekday: 'short' }).format(new Date(1970, 0, i + 4))
-  );
+    // Get browser's locale for formatting
+    const browserLocale = navigator.language;
 
-  // Styles
-  const styles = `
-  <style>
-    #calendar-grid {
-      display: grid;
-      grid-template-columns: repeat(7, 1fr);
-      gap: 10px;
-      text-align: center;
-    }
-    .calendar-heading {
-      font-weight: bold;
-      padding-bottom: 5px;
-    }
-    .day-cell {
-      padding: 5px;
-      transition: background-color 0.3s;
-    }
-    .day-cell.today {
-      border: 2px solid #ff0000; /* Highlight today's date */
-    }
-  </style>
-  `;
+    // Generate weekdays based on the locale, starting with Sunday
+    const weekdays = [...Array(7).keys()].map((i) =>
+      new Intl.DateTimeFormat(browserLocale, { weekday: 'short' }).format(new Date(1970, 0, i + 4))
+    );
 
-  // Header
-  const header = `<h1>${new Intl.DateTimeFormat(browserLocale, { month: 'long' }).format(
-    currentDate
-  )} ${year}</h1>`;
+    // Styles
+    const styles = `
+    <style>
+      #calendar-grid {
+        display: grid;
+        grid-template-columns: repeat(7, 1fr);
+        gap: 10px;
+        text-align: center;
+      }
+      .calendar-heading {
+        font-weight: bold;
+        padding-bottom: 5px;
+      }
+      .day-cell {
+        padding: 5px;
+        transition: background-color 0.3s;
+      }
+      .day-cell.today {
+        border: 2px solid #ff0000; /* Highlight today's date */
+      }
+      .calendar-nav {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+      }
+    </style>
+    `;
 
-  // Weekday headers
-  const weekdaysHeader = weekdays.map((day) => `<div class="calendar-heading">${day}</div>`).join('');
+    // Navigation
+    const nav = `
+      <div class="calendar-nav">
+        <button id="prev-month">&lt; Prev</button>
+        <strong>${new Intl.DateTimeFormat(browserLocale, { month: 'long' }).format(
+          currentDate
+        )} ${year}</strong>
+        <button id="next-month">Next &gt;</button>
+      </div>
+    `;
 
-  // Empty cells before the first day of the month
-  const emptyCells = Array.from({ length: firstDayOfMonth })
-    .map(() => '<div class="day-cell"></div>')
-    .join('');
+    // Weekday headers
+    const weekdaysHeader = weekdays.map((day) => `<div class="calendar-heading">${day}</div>`).join('');
 
-  // Day cells for the month
-  const daysCells = Array.from({ length: daysInMonth })
-    .map((_, i) => {
-      const day = i + 1;
-      const isToday = day === currentDay ? 'today' : '';
-      return `<div class="day-cell ${isToday}">${day}</div>`;
-    })
-    .join('');
+    // Empty cells before the first day of the month
+    const emptyCells = Array.from({ length: firstDayOfMonth })
+      .map(() => '<div class="day-cell"></div>')
+      .join('');
 
-  // Full grid
-  const calendarGrid = `<div id="calendar-grid">${weekdaysHeader}${emptyCells}${daysCells}</div>`;
+    // Day cells for the month
+    const daysCells = Array.from({ length: daysInMonth })
+      .map((_, i) => {
+        const day = i + 1;
+        const isToday =
+          day === new Date().getDate() && month === new Date().getMonth() && year === new Date().getFullYear()
+            ? 'today'
+            : '';
+        return `<div class="day-cell ${isToday}">${day}</div>`;
+      })
+      .join('');
 
-  // Combine all parts and set as innerHTML of the host component
-  hostComponent.innerHTML = `${styles}${header}${calendarGrid}`;
+    // Full grid
+    const calendarGrid = `<div id="calendar-grid">${weekdaysHeader}${emptyCells}${daysCells}</div>`;
+
+    // Combine all parts and set as innerHTML of the host component
+    hostComponent.innerHTML = `${styles}${nav}${calendarGrid}`;
+
+    // Add event listeners for navigation
+    document.getElementById('prev-month').addEventListener('click', () => {
+      currentDate.setMonth(currentDate.getMonth() - 1);
+      updateCalendar(hostComponent, currentDate);
+    });
+
+    document.getElementById('next-month').addEventListener('click', () => {
+      currentDate.setMonth(currentDate.getMonth() + 1);
+      updateCalendar(hostComponent, currentDate);
+    });
+  };
+
+  updateCalendar(hostComponent, currentDate);
 };
