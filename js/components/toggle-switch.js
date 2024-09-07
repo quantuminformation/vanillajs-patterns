7c0f5b26187a5fd756fa6fc7eb40aae09721f62f
Nikos Katsikanis <nkatsikains@gmail.com>
2024-09-07 14:45:59 +0200

    initial toggle switch

---
+++ js/components/toggle-switch.js
@@ -0,0 +1,21 @@
+// Stored in /components/toggle-switch.js
+
+export default (hostComponent) => {
+  const render = () => {
+    hostComponent.innerHTML = `
+      <label class="switch">
+        <input type="checkbox" id="toggle">
+        <span class="slider round"></span>
+      </label>
+    `;
+
+    const toggleInput = hostComponent.querySelector('#toggle');
+    toggleInput.addEventListener('change', () => {
+      const isChecked = toggleInput.checked;
+      console.log('Toggle status:', isChecked);
+    });
+  };
+
+  // Initial render
+  render();
+};
